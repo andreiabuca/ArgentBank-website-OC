9c688f57fc0e2c55681eb36a2b0b606433d2101c
Andreia Buca <andreia.buca@hotmail.com>
2024-08-03 05:14:47 +0200

    convert user page html to react

--- argentbank-oc/src/App.js
+++ argentbank-oc/src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './assets/css/main.css';
 import NavBar from './components/navbar';
 import Footer from './components/footer';
-import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <Home/>
       <Footer/>
     </div>
   );
