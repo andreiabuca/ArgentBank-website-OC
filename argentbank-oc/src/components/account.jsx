cc304ae98782b01e855f97ea604e4a962d570c11
Andreia Buca <andreia.buca@hotmail.com>
2024-08-08 01:29:27 +0200

    add edit name form

--- argentbank-oc/src/components/account.jsx
+++ argentbank-oc/src/components/account.jsx
@@ -1,19 +1,26 @@
 import React from "react";
-import '../assets/css/main.css'; 
+import '../assets/css/main.css';
 
-const Account = ({title, amount, description}) => {
-    return (
-        <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">{title}</h3>
-          <p className="account-amount">{amount}</p>
-          <p className="account-amount-description">{description}</p>
-        </div>
+const Account = ({ title, amount, description, showTransactionsButton = true, customClass, showProfileArrow = false }) => {
+  return (
+    <section className={`account ${customClass}`}>
+      <div className="account-content-wrapper">
+        <h3 className="account-title">{title}</h3>
+        <p className="account-amount">{amount}</p>
+        <p className="account-amount-description">{description}</p>
+      </div>
+      {showTransactionsButton && (
         <div className="account-content-wrapper cta">
           <button className="transaction-button">View transactions</button>
         </div>
-      </section>
-    );
+      )}
+      {showProfileArrow && (
+        <div className="arrow-button">
+          <i class="fa-solid fa-chevron-right"></i>
+        </div>
+      )}
+    </section>
+  );
 };
 
 export default Account;
